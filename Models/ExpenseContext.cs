736b3bdb2c588cb40b65520bdcbb34c5818fe9f5
Maria Volintiru <maria.volintiru@yahoo.com>
2022-11-29 19:32:24 +0200

    Add project files.

---
+++ Models/ExpenseContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Volintiru_Andreea_Lab6.Models
+{
+    public class ExpenseContext : DbContext
+
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options): base(options)
+        {
+        }
+        public DbSet<Expense> Expense { get; set; }
+    }
+}
---
+++ Models/Expenses.cs
@@ -0,0 +1,11 @@
+﻿namespace Volintiru_Andreea_Lab6.Models
+{
+    public class Expense
+    {
+        
+ public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+    }
+}
